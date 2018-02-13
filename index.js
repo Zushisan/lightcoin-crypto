1f0490e80e1473572cbc367683b3824d1ee053ae
Zushisan <74838@supinfo.com>
2018-02-13 21:17:26 +0000

    lightcoin exercise with with transaction history and transaction check

--- index.js
+++ index.js
@@ -1,29 +1,96 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+
+    if(this.isAllowed()){
+      this.time = new Date();
+      this.account.addTransaction(this);
+    }
+    else {
+      console.log('Cannot proceed, insuficiant funds.')
+    }
+
   }
 
 }
 
+class Deposit extends Transaction {
 
+  get value() {
+    return this.amount;
+  }
 
+  isAllowed() {
+    return true;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -(this.amount);
+  }
+
+  isAllowed() {
+
+    if (this.account.balance >= this.amount) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.transaction = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for(let type of this.transaction){
+      balance += type.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transaction.push(transaction);
+  }
+
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("Romain-bank");
 
-t1 = new Withdrawal(50.25);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+// console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+// console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit (728, myAccount);
+t3.commit()
+// console.log('Transaction 3:', t3);
+
+t4 = new Withdrawal(9.99, myAccount);
+t4.commit();
+// console.log('Transaction 2:', t2);
+
+console.log('Balance:', myAccount.balance);
+// console.log('Transaction: ', myAccount.transaction);
